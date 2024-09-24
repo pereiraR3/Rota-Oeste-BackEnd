603cd7e36f629242223bfecfdd4cb0470c46856c
pereira3r <anthony_rodriguespereira@outlook.com>
2024-09-24 09:21:29 -0400

    fix: edit. AutoMapperProfile.cs with mapper to update entity

--- api-rota-oeste/Data/AutoMapper/AutoMapperProfile.cs
+++ api-rota-oeste/Data/AutoMapper/AutoMapperProfile.cs
@@ -1,5 +1,4 @@
 using AutoMapper;
-using api_rota_oeste.Models;
 using api_rota_oeste.Models.Cliente;
 using api_rota_oeste.Models.Usuario;
 public class AutoMapperProfile : Profile
@@ -11,5 +10,9 @@ public class AutoMapperProfile : Profile
             
         CreateMap<UsuarioModel, UsuarioResponseDTO>();
             
+        // O mapeamento de atualização deve ignorar campos nulos
+        CreateMap<UsuarioPatchDTO, UsuarioModel>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
     }
 }
