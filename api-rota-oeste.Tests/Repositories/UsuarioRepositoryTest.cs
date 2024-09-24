32aa896481513c9e5fd5073399631ddc0ad25a18
pereira3r <anthony_rodriguespereira@outlook.com>
2024-09-24 09:23:45 -0400

    fix: create Unit Test to UsuarioRepositoryTest.cs

--- api-rota-oeste.Tests/Repositories/UsuarioRepositoryTest.cs
+++ api-rota-oeste.Tests/Repositories/UsuarioRepositoryTest.cs
@@ -28,6 +28,8 @@ namespace api_rota_oeste.Tests.Repositories
             {
                 cfg.CreateMap<UsuarioModel, UsuarioResponseDTO>();
                 cfg.CreateMap<UsuarioRequestDTO, UsuarioModel>();
+                cfg.CreateMap<UsuarioPatchDTO, UsuarioModel>()
+                    .ForAllMembers(ops => ops.Condition((src, dest, srcMember) => srcMember != null));
             });
 
             _mapper = mapperConfig.CreateMapper();
@@ -60,5 +62,163 @@ namespace api_rota_oeste.Tests.Repositories
             Assert.NotNull(usuarioNoBanco);
             Assert.Equal("teste user", usuarioNoBanco.Nome);
         }
+
+        [Fact]
+        public async Task BuscarPorId_DeveRetornarUsuarioModel()
+        {
+            
+            // Arrange: Criar o request DTO com dados fictícios
+            var usuarioRequest = new UsuarioRequestDTO(
+                "66992337652", 
+                "teste user",
+                "122123",
+                null
+            );
+            
+            // Act: Adicionar o usuário ao banco de dados em memória
+            var result = await _usuarioRepository.Adicionar(usuarioRequest);
+            var usuarioModel = await _usuarioRepository.BuscaPorId(result.Id);
+            
+            //Assert: verifica se o retorno é o esperado
+            Assert.NotNull(usuarioModel);
+            Assert.Equal(usuarioRequest.Nome, usuarioModel.Nome);
+            Assert.Equal(usuarioRequest.Telefone, usuarioModel.Telefone);
+
+        }
+
+        [Fact]
+        public async Task BuscarPorId_DeveRetornarNotFound()
+        {
+            
+            // Arrange: Criar o request DTO com dados fictícios
+            
+            /*
+             * Neste caso não haverá dados, pois vamos forçar um Not Found
+             */
+            
+            // Act: Adicionar o usuário ao banco de dados em memória
+            var usuarioModel = await _usuarioRepository.BuscaPorId(1);
+            
+            // Assert: verifica se o retorno é o esperado
+            Assert.Null(usuarioModel);
+            
+        }
+
+        [Fact]
+        public async Task Atualizar_DeveRetornarTrue()
+        {
+            // Arrange: Criar o request DTO com dados fictícios
+            var usuarioRequest = new UsuarioRequestDTO(
+                "66992337652",
+                "teste user",
+                "122123",
+                null
+            );
+    
+            // Act: Adicionar o usuário ao banco de dados em memória
+            var result = await _usuarioRepository.Adicionar(usuarioRequest);
+    
+            // Atualizar parcialmente o usuário com um patch
+            var usuarioPatch = new UsuarioPatchDTO
+            {
+                Id = result.Id,
+                Telefone = "66992337652",  // Mantemos o telefone o mesmo
+                Nome = null,               // Nome não será atualizado
+                Foto = null                // Foto também não será atualizada
+            };
+    
+            // Executa a atualização parcial
+            var resultPatch = await _usuarioRepository.Atualizar(usuarioPatch);
+    
+            // Recupera o usuário atualizado do banco de dados
+            var usuarioBanco = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.Id == result.Id);
+    
+            // Assert: Verifica se o retorno é o esperado
+            Assert.NotNull(result);
+            Assert.True(resultPatch);
+            Assert.Equal(usuarioPatch.Telefone, usuarioBanco.Telefone);
+            Assert.Equal(usuarioRequest.Nome, usuarioBanco.Nome);
+        }
+        
+        [Fact]
+        public async Task Atualizar_DeveRetornarFalse()
+        {
+            
+            // Arrange: Criar o request DTO com dados fictícios
+            var usuarioRequest = new UsuarioRequestDTO(
+                "66992337652",
+                "teste user",
+                "122123",
+                null
+            );
+    
+            // Act: Adicionar o usuário ao banco de dados em memória
+            var result = await _usuarioRepository.Adicionar(usuarioRequest);
+    
+            // Atualizar parcialmente o usuário com um patch
+            var usuarioPatch = new UsuarioPatchDTO
+            {
+                Id = 100,
+                Telefone = "66992337652",  // Mantemos o telefone o mesmo
+                Nome = null,               // Nome não será atualizado
+                Foto = null                // Foto também não será atualizada
+            };
+    
+            // Executa a atualização parcial
+            var resultPatch = await _usuarioRepository.Atualizar(usuarioPatch);
+            
+            // Assert: Verifica se o retorno é o esperado      
+            Assert.False(resultPatch);
+            
+        }
+        
+        [Fact]
+        public async Task Apagar_DeveRetornarTrue()
+        {
+
+            // Arrange: Criar o request DTO com dados fictícios
+            var usuarioRequest = new UsuarioRequestDTO(
+                "66992337652",
+                "teste user",
+                "122123",
+                null
+            );
+
+            // Act: Adicionar o usuário ao banco de dados em memória e removê-lo
+            var result = await _usuarioRepository.Adicionar(usuarioRequest);
+            var statusApagar = await _usuarioRepository.Apagar(result.Id);
+            var usuarioBanco = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.Telefone == "66992337652");
+
+            // Assert: verifica se o retorno é o esperado
+            Assert.True(statusApagar);
+            Assert.Null(usuarioBanco);
+
+        }
+
+        [Fact]
+        public async Task Apagar_DeveRetornarFalse()
+        {
+            
+            // Arrange: Criar o request DTO com dados fictícios
+            var usuarioRequest = new UsuarioRequestDTO(
+                "66992337652",
+                "teste user",
+                "122123",
+                null
+            );
+
+            // Act: Adicionar o usuário ao banco de dados em memória e removê-lo
+            var result = await _usuarioRepository.Adicionar(usuarioRequest);
+            var statusApagar = await _usuarioRepository.Apagar(3);
+            var usuarioBanco = await _dbContext.Usuarios.FirstOrDefaultAsync(u => u.Id == 1);
+            
+            // Assert: verifica se o retorno é o esperado
+            Assert.NotNull(result);
+            Assert.False(statusApagar);
+            Assert.NotNull(usuarioBanco);
+            
+        }
+        
+
     }
 }
