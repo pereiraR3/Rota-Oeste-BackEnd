cee339a2553593edf4bf3a265671a34925c30345
andre arce <andrearce343@gmail.com>
2024-09-24 21:30:24 -0400

    Criação da classe CheckListMap e modificações no contexto e AutoMapper

--- api-rota-oeste/Models/CheckList/CheckListRequestDTO.cs
+++ api-rota-oeste/Models/CheckList/CheckListRequestDTO.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace api_rota_oeste.Models.CheckList;
 
-namespace api_rota_oeste.Models.CheckList
-{
-    public record CheckListRequestDTO
-    (
-
-    );
-}
+public record CheckListRequestDTO(
+    string? Nome,
+    DateTime DataCriacao,
+    int UsuarioId
+);
