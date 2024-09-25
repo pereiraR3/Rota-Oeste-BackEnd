cdb8ffc410bc8a4e5df37d1d49b223124f621e2b
andre arce <andrearce343@gmail.com>
2024-09-24 21:30:24 -0400

    checklistModel

--- api-rota-oeste/Models/CheckList/CheckListModel.cs
+++ api-rota-oeste/Models/CheckList/CheckListModel.cs
@@ -1,3 +1,4 @@
+using api_rota_oeste.Models.Usuario;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,19 @@ namespace api_rota_oeste.Models.CheckList
 {
     public class CheckListModel
     {
-        
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual UsuarioModel? Usuario { get; set; }
+
+        public CheckListModel(CheckListRequestDTO req, UsuarioModel? usuario)
+        {
+            this.Nome = req.Nome;
+            this.DataCriacao = req.DataCriacao;
+            this.Usuario = usuario;
+            this.UsuarioId = req.UsuarioId;
+            
+        }
     }
 }
