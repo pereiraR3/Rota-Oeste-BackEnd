9712f45a2ca7033b62b943e9785d55fb16ef4080
andre arce <andrearce343@gmail.com>
2024-09-27 17:30:15 -0400

    Update CheckListModel.cs

--- api-rota-oeste/Models/CheckList/CheckListModel.cs
+++ api-rota-oeste/Models/CheckList/CheckListModel.cs
@@ -1,3 +1,4 @@
+using api_rota_oeste.Models.Usuario;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,20 @@ namespace api_rota_oeste.Models.CheckList
 {
     public class CheckListModel
     {
-        
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public int UsuarioId { get; set; }
+        public virtual UsuarioModel? Usuario { get; set; }
+
+        public CheckListModel() { }
+        public CheckListModel(CheckListRequestDTO req, UsuarioModel? usuario)
+        {
+            this.Nome = req.Nome;
+            this.DataCriacao = DateTime.Now; //a dataCriacao do Checklist � definida como a data no momento da criacao
+            this.Usuario = usuario;
+            this.UsuarioId = req.UsuarioId;
+            
+        }
     }
 }
