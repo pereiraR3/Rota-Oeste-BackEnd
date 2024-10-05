27cfeacd5da911aa6b4f9eb2aea7de28c108f1ee
pereira3r <anthony_rodriguespereira@outlook.com>
2024-10-05 15:33:48 -0400

    fix: edit AlternativaResponseDTO.cs

--- api-rota-oeste/Models/Alternativa/AlternativaResponseDTO.cs
+++ api-rota-oeste/Models/Alternativa/AlternativaResponseDTO.cs
@@ -1,17 +1,30 @@
 using api_rota_oeste.Models.Questao;
+using api_rota_oeste.Models.RespostaTemAlternativa;
 
 namespace api_rota_oeste.Models.Alternativa;
 
-public record AlternativaResponseDTO(
-    
-    int Id,
-    
-    int QuestaoId,
-    
-    string Descricao,
-    
-    int Codigo,
-    
-    QuestaoModel? Questao
+public class AlternativaResponseDTO
+{
+    public int Id { get; set; }
+    public int QuestaoId { get; set; }
+    public string Descricao { get; set; }
+    public int Codigo { get; set; }
+    public QuestaoResponseDTO? Questao { get; set; }
+    public List<RespostaTemAlternativaResponseDTO>? RespostaTemAlternativaResponseDtos { get; set; }
 
-);
+    // Construtor sem argumentos
+    public AlternativaResponseDTO() { }
+
+    // Construtor completo (opcional)
+    public AlternativaResponseDTO(int id, int questaoId, string descricao, int codigo, 
+        QuestaoResponseDTO? questao, 
+        List<RespostaTemAlternativaResponseDTO>? respostaTemAlternativaResponseDtos)
+    {
+        Id = id;
+        QuestaoId = questaoId;
+        Descricao = descricao;
+        Codigo = codigo;
+        Questao = questao;
+        RespostaTemAlternativaResponseDtos = respostaTemAlternativaResponseDtos;
+    }
+}
