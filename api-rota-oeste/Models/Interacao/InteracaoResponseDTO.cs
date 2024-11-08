c4fe4cc95d8d294524f03c0f51e77133bb5d575e
Siegepan <anthony_rodriguespereira@outlook.com>
2024-11-08 17:39:41 -0400

    fix: edit InteracaoResponseDTO.cs.cs to be manipulated for AutoMapper

--- api-rota-oeste/Models/Interacao/InteracaoResponseDTO.cs
+++ api-rota-oeste/Models/Interacao/InteracaoResponseDTO.cs
@@ -20,21 +20,34 @@ namespace api_rota_oeste.Models.Interacao;
 /// <seealso cref="CheckListResponseDTO"/>
 /// <seealso cref="RespostaResponseDTO"/>
 public record InteracaoResponseDTO
-(
-    
-    int Id,
-    
-    int ClienteId,
-    
-    int CheckListId,
-    
-    bool Status,
-    
-    ClienteResponseDTO? Cliente,
-    
-    CheckListResponseDTO? CheckList,
-    
-    List<RespostaResponseDTO>? RespostaAlternativaModels
-    
-);
+{
+    public int Id { get; init; }
+    public int ClienteId { get; init; }
+    public int CheckListId { get; init; }
+    public bool Status { get; init; }
+    public ClienteResponseDTO? Cliente { get; init; }
+    public CheckListResponseDTO? CheckList { get; init; }
+    public List<RespostaResponseDTO>? RespostaAlternativaModels { get; init; }
 
+    // Construtor vazio para AutoMapper
+    public InteracaoResponseDTO() {}
+
+    // Construtor principal
+    public InteracaoResponseDTO(
+        int id,
+        int clienteId,
+        int checkListId,
+        bool status,
+        ClienteResponseDTO? cliente,
+        CheckListResponseDTO? checkList,
+        List<RespostaResponseDTO>? respostaAlternativaModels)
+    {
+        Id = id;
+        ClienteId = clienteId;
+        CheckListId = checkListId;
+        Status = status;
+        Cliente = cliente;
+        CheckList = checkList;
+        RespostaAlternativaModels = respostaAlternativaModels;
+    }
+}
